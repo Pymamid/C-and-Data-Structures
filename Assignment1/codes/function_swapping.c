149c042b3ca67145d73d9c49005ea69b9db1537a
Pymamid <86880569+Pymamid@users.noreply.github.com>
2021-09-18 12:52:21 +0530

    Add files via upload

---
+++ Assignment1/codes/function_swapping.c
@@ -0,0 +1,33 @@
+
+/*
+The main function passes two strings each to fun1 and fun2 and prints the outputs.
+
+fun1 takes in the pointers to the two strings, and swaps them. But since the values get copied to the parameters, the actual strings don't get swapped.
+
+fun2 takes in the address of the pointers of the two strings. Since fun2 dereferences it one level before swapping, the strings get swapped.
+*/
+
+#include<stdio.h>
+
+void fun1(char *s1, char *s2){
+	char *tmp;
+	tmp = s1;
+	s1 = s2;
+	s2 = tmp;
+}
+
+void fun2(char **s1, char **s2){
+	char *tmp;
+	tmp = *s1;
+	*s1 = *s2;
+	*s2 = tmp;
+}
+
+int main(){
+	char *str1 = ”Hi”, *str2 = ”Bye”;
+	fun1(str1, str2);
+	printf(”%s %s ”, str1, str2);
+	fun2(&str1, &str2);
+	printf(”%s %s”, str1, str2);
+	return 0;
+}
